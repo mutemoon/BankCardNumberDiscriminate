c30c6546d4dac61bc7a5d84dd51e216b3dd0a408
Haoran Zhao <1036066414@qq.com>
2019-04-10 20:56:53 +0800

    update

--- main.py
+++ main.py
@@ -1,86 +1,39 @@
-# encoding=utf-8
 import tensorflow as tf
+import os
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+data_path = r'.\data\images'
+image_batch = []
+label_batch = []
+input_num = 20
+i = 0
+for f in os.listdir(data_path):
+    image_batch.append(data_path + "\\" + f)
+    label_batch.append([1, 0])
+    if i > input_num:
+        break
+    i += 1
+
+image = tf.cast(image_batch, tf.string)
+label = tf.cast(label_batch, tf.int32)
+input_queue = tf.train.slice_input_producer([image, label])
+image = input_queue[0]
+label = input_queue[1]
+image = tf.read_file(image)
+image = tf.image.decode_jpeg(image, channels=3)
+image = tf.image.resize_image_with_crop_or_pad(image, 120, 46)
+image_batch, label_batch = tf.train.batch([image, label], batch_size=10, num_threads=64, capacity=1000)
+image_batch = tf.cast(image_batch, tf.float32)
+label_batch = tf.cast(label_batch, tf.float32)
+print("data load successful...")
 
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)  # 截断正态分布，此函数原型为尺寸、均值、标准差
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')  # strides第0位和第3为一定为1，剩下的是卷积的横向和纵向步长
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 参数同上，ksize是池化块的大小
-
-
-x = tf.placeholder("float", shape=[None, 784])
-y_ = tf.placeholder("float", shape=[None, 10])
-
-# 图像转化为一个四维张量，第一个参数代表样本数量，-1表示不定，第二三参数代表图像尺寸，最后一个参数代表图像通道数
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-# 第一层卷积加池化
-w_conv1 = weight_variable([5, 5, 1, 32])  # 第一二参数值得卷积核尺寸大小，即patch，第三个参数是图像通道数，第四个参数是卷积核的数目，代表会出现多少个卷积特征
-b_conv1 = bias_variable([32])
-
-h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# 第二层卷积加池化
-w_conv2 = weight_variable([5, 5, 32, 64])  # 多通道卷积，卷积出64个特征
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# 原图像尺寸28*28，第一轮图像缩小为14*14，共有32张，第二轮后图像缩小为7*7，共有64张
-
-w_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])  # 展开，第一个参数为样本数量，-1未知
-f_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
-
-# dropout操作，减少过拟合
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(f_fc1, keep_prob)
-
-w_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
-
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))  # 定义交叉熵为loss函数
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)  # 调用优化器优化
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-sess = tf.InteractiveSession()
-sess.run(tf.initialize_all_variables())
-for i in range(2000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images[0:500], y_: mnist.test.labels[0:500], keep_prob: 1.0}))
-
-
-model = {
+model_info = {
     'learning_rate': 0.0002,
     'input_layer_batch_size': 10,
-    'input_layer_image_shape': [28, 28],
+    'input_layer_image_shape': [120, 46],
     'input_layer_image_channel': 3,
     'conv_and_pool_layer_num': 2,
     'conv_and_pool_layer_filter_ksize': [
@@ -108,48 +61,40 @@ model = {
 
 
 class CnnModel:
-    def __init__(self, model_shape):
-        self.sess = tf.Session()
+    def __init__(self, model_shape, image_batch, label_batch):
         self.model_info = model_shape
-
-        self.input_batch = None
-        self.label_batch = None
+        self.image_batch = image_batch
+        self.label_batch = label_batch
         self.output = None
         self.loss = None
-        self.train = None
+        self.minimizer = None
         self.accuracy = None
         self.create_model()
 
         self.sess_init = tf.global_variables_initializer()
+        self.sess = tf.Session()
         self.sess.run(self.sess_init)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=self.sess, coord=coord)
 
     def create_model(self):
-        batch_shape = [
-            self.model_info['input_layer_batch_size'],
-            self.model_info['input_layer_image_shape'][0],
-            self.model_info['input_layer_image_shape'][1],
-            self.model_info['input_layer_image_channel']
-        ]
-
-        self.input_batch = tf.placeholder(dtype=tf.float32, shape=batch_shape)
-        self.label_batch = tf.placeholder(dtype=tf.float32, shape=[self.model_info['input_layer_batch_size'], self.model_info["output_layer_units"]])
-
-        layer_input = self.input_batch
-        for layer_id in self.model_info['conv_and_pool_layer_num']:
+        layer_input = self.image_batch
+        for layer_id in range(self.model_info['conv_and_pool_layer_num']):
             layer_input = self.create_conv_and_pool_layer(layer_id, layer_input)
 
-        for layer_id in self.model_info['fully_connected_layer_num']:
-            layer_input = self.create_fully_connected_layer(layer_id, layer_input)
+        layer_input = tf.reshape(layer_input, [self.model_info['input_layer_batch_size'], -1])
 
+        for layer_id in range(self.model_info['fully_connected_layer_num']):
+            layer_input = self.create_fully_connected_layer(layer_id, layer_input)
         self.output = self.create_output_layer(layer_input)
         self.loss = -tf.reduce_sum(self.label_batch * tf.log(self.output))
-        self.train = tf.train.AdamOptimizer(self.model_info['learning_rate']).minimize(self.loss)
-        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(self.output, 1), tf.arg_max(self.label_batch, 1)), 'float'))
+        self.minimizer = tf.train.AdamOptimizer(self.model_info['learning_rate']).minimize(self.loss)
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.math.argmax(self.output, 1), tf.math.argmax(self.label_batch, 1)), 'float'))
 
     def create_conv_and_pool_layer(self, layer_id, layer_input):
         conv_ksize = [
-            self.model_info["conv_and_pool_layer_filter_shape"][layer_id][0],
-            self.model_info["conv_and_pool_layer_filter_shape"][layer_id][1],
+            self.model_info["conv_and_pool_layer_filter_ksize"][layer_id][0],
+            self.model_info["conv_and_pool_layer_filter_ksize"][layer_id][1],
             self.model_info["input_layer_image_channel"],
             self.model_info["conv_and_pool_layer_filter_cores"][layer_id]
         ]
@@ -206,6 +151,89 @@ class CnnModel:
                                      tf.nn.softmax,
                                      kernel_initializer=tf.random_normal_initializer(stddev=0.1, dtype=tf.float32),
                                      bias_initializer=tf.constant_initializer(0.1))
+            print(layer_input.shape)
+            print(output.shape)
         return output
 
     def train(self):
+        print("start training...")
+        for i in range(2000):
+            _, loss, accuracy = self.sess.run([self.minimizer, self.loss, self.accuracy])
+            print(i, accuracy, loss)
+
+
+cnn = CnnModel(model_info, image_batch, label_batch)
+cnn.train()
+
+# mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+#
+#
+# def weight_variable(shape):
+#     initial = tf.truncated_normal(shape, stddev=0.1)  # 截断正态分布，此函数原型为尺寸、均值、标准差
+#     return tf.Variable(initial)
+#
+#
+# def bias_variable(shape):
+#     initial = tf.constant(0.1, shape=shape)
+#     return tf.Variable(initial)
+#
+#
+# def conv2d(x, W):
+#     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')  # strides第0位和第3为一定为1，剩下的是卷积的横向和纵向步长
+#
+#
+# def max_pool_2x2(x):
+#     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 参数同上，ksize是池化块的大小
+#
+#
+# x = tf.placeholder("float", shape=[None, 784])
+# y_ = tf.placeholder("float", shape=[None, 10])
+#
+# # 图像转化为一个四维张量，第一个参数代表样本数量，-1表示不定，第二三参数代表图像尺寸，最后一个参数代表图像通道数
+# x_image = tf.reshape(x, [-1, 28, 28, 1])
+#
+# # 第一层卷积加池化
+# w_conv1 = weight_variable([5, 5, 1, 32])  # 第一二参数值得卷积核尺寸大小，即patch，第三个参数是图像通道数，第四个参数是卷积核的数目，代表会出现多少个卷积特征
+# b_conv1 = bias_variable([32])
+#
+# h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+# h_pool1 = max_pool_2x2(h_conv1)
+#
+# # 第二层卷积加池化
+# w_conv2 = weight_variable([5, 5, 32, 64])  # 多通道卷积，卷积出64个特征
+# b_conv2 = bias_variable([64])
+#
+# h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
+#
+# # 原图像尺寸28*28，第一轮图像缩小为14*14，共有32张，第二轮后图像缩小为7*7，共有64张
+#
+# w_fc1 = weight_variable([7 * 7 * 64, 1024])
+# b_fc1 = bias_variable([1024])
+#
+# h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])  # 展开，第一个参数为样本数量，-1未知
+# f_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
+#
+# # dropout操作，减少过拟合
+# keep_prob = tf.placeholder(tf.float32)
+# h_fc1_drop = tf.nn.dropout(f_fc1, keep_prob)
+#
+# w_fc2 = weight_variable([1024, 10])
+# b_fc2 = bias_variable([10])
+# y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+#
+# cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))  # 定义交叉熵为loss函数
+# train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)  # 调用优化器优化
+# correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+#
+# sess = tf.InteractiveSession()
+# sess.run(tf.initialize_all_variables())
+# for i in range(2000):
+#     batch = mnist.train.next_batch(50)
+#     if i % 100 == 0:
+#         train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+#         print("step %d, training accuracy %g" % (i, train_accuracy))
+#     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+#
+# print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images[0:500], y_: mnist.test.labels[0:500], keep_prob: 1.0}))
--- test.py
+++ test.py
@@ -1,16 +1,38 @@
 import tensorflow as tf
+import os
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+data_path = r'.\data\images'
+image_batch = []
+label_batch = []
+input_num = 100
+i = 0
+for f in os.listdir(data_path):
+    i += 1
+    if i > input_num:
+        break
+    input_file = data_path + "\\" + f
+    image = tf.image.decode_png(tf.read_file(input_file), 3)
+    image = tf.image.resize_image_with_crop_or_pad(image, 120, 46)
+    image_batch.append(image)
+    label_batch.append([1, 0])
+print(image_batch)
 
 
-out = tf.constant([[[10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 5], [0, 0, 0, 0, 0, 0]], [[10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 0]]], dtype=tf.float32)
-softmax_out = tf.nn.softmax(out)
-out_clipped = tf.clip_by_value(softmax_out, 1e-10, 0.999999)
-
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    o = sess.run(softmax_out)
-    print(list(o))
-    o = sess.run(out_clipped)
-    print(list(o))
-    o = sess.run(tf.reduce_mean(out_clipped))
-    print(o)
+# import tensorflow as tf
+#
+#
+# out = tf.constant([[[10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 5], [0, 0, 0, 0, 0, 0]], [[10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 5], [10, 10, 2, 3, 4, 0]]], dtype=tf.float32)
+# softmax_out = tf.nn.softmax(out)
+# out_clipped = tf.clip_by_value(softmax_out, 1e-10, 0.999999)
+#
+# init = tf.global_variables_initializer()
+# with tf.Session() as sess:
+#     sess.run(init)
+#     o = sess.run(softmax_out)
+#     print(list(o))
+#     o = sess.run(out_clipped)
+#     print(list(o))
+#     o = sess.run(tf.reduce_mean(out_clipped))
+#     print(o)
