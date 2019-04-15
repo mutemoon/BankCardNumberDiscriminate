11d3feb706afe4beda498c713fe407a82677744f
Haoran Zhao <1036066414@qq.com>
2019-04-15 10:42:09 +0800

    update

--- CnnModel.py
+++ CnnModel.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
-import os
-from PIL import Image
 from pylab import *
+
 
 MODEL_INFO = {
     'learning_rate': 0.001,
-    'input_layer_batch_size': 100,
+    'input_layer_batch_size': 128,
     'input_layer_image_shape': [20, 20],
     'input_layer_image_channels': 1,
     'conv_and_pool_layer_num': 2,
@@ -53,8 +52,6 @@ class CnnModel:
         labels = []
         i = 0
         for f in os.listdir(self.data_path):
-            # if int(f.split("-")[1].split(".")[0]) > 3282:
-            #     continue
             image_paths.append(self.data_path + "\\" + f)
             if f.split("-")[0] == "0":
                 labels.append(0)
@@ -75,20 +72,26 @@ class CnnModel:
 
         image = tf.read_file(image)
         image = tf.image.decode_jpeg(image, channels=3)
-        image = tf.image.rgb_to_grayscale(image)
+        if self.model_info['input_layer_image_channels'] == 1:
+            image = tf.image.rgb_to_grayscale(image)
         image = tf.image.resize_image_with_crop_or_pad(image, self.model_info['input_layer_image_shape'][0],
                                                        self.model_info['input_layer_image_shape'][1])
+        image, label = tf.train.batch([image, label],
+                                      batch_size=self.model_info['input_layer_batch_size'],
+                                      num_threads=64,
+                                      capacity=1 + self.model_info['input_layer_batch_size'])
 
-        image_paths, labels = tf.train.batch([image, label],
-                                             batch_size=self.model_info['input_layer_batch_size'],
-                                             num_threads=64,
-                                             capacity=1 + self.model_info['input_layer_batch_size'])
-
-        self.image_batch = tf.cast(image_paths, tf.float32)
-        self.label_batch = tf.cast(labels, tf.int32)
+        self.image_batch = tf.cast(image, tf.float32)
+        self.label_batch = tf.cast(label, tf.int32)
         print("batch load successful...")
 
     def create_model(self):
+        if self.image_batch is None and self.label_batch is None:
+            self.image_batch = tf.placeholder(dtype=tf.float32, shape=[self.model_info['input_layer_batch_size'],
+                                                                       self.model_info['input_layer_image_shape'][0],
+                                                                       self.model_info['input_layer_image_shape'][1],
+                                                                       self.model_info['input_layer_image_channels']])
+            self.label_batch = tf.placeholder(dtype=tf.int32, shape=[self.model_info['input_layer_batch_size']])
         layer_input = self.image_batch
         for layer_id in range(self.model_info['conv_and_pool_layer_num']):
             layer_input = self.create_conv_and_pool_layer(layer_id, layer_input)
@@ -159,7 +162,6 @@ class CnnModel:
             conv = tf.nn.conv2d(layer_input, weight, strides=conv_strides, padding="SAME")
             conv = tf.nn.relu(tf.nn.bias_add(conv, bias))
             pool = tf.nn.max_pool(conv, ksize=pool_ksize, strides=pool_strides, padding="SAME")
-
         return pool
 
     def create_fully_connected_layer(self, layer_id, layer_input):
@@ -179,111 +181,3 @@ class CnnModel:
                                      kernel_initializer=tf.random_normal_initializer(stddev=0.1, dtype=tf.float32),
                                      bias_initializer=tf.constant_initializer(0.1))
         return output
-
-    def train(self):
-        print("start training...")
-        epoch_accuracy = 0
-        epoch_step = 0
-        for step in range(1, 100000):
-            _, loss, accuracy = self.sess.run([self.minimizer, self.loss, self.accuracy])
-            epoch_accuracy += float(accuracy)
-
-            if step % 20 == 0:
-                print("step:" + str(step), epoch_accuracy / epoch_step, loss)
-
-            if step % 500 == 0:
-                self.saver.save(self.sess, self.model_path + r'\model.ckpt', global_step=step)
-                epoch_accuracy = 0
-                epoch_step = 0
-            epoch_step += 1
-
-
-class Trainer:
-    def __init__(self, model_info):
-        self.cnn = CnnModel(model_info)
-
-    def start_train(self):
-        image_paths, labels = self.cnn.get_image_paths_and_labels_from_image_files()
-        self.cnn.create_batch(image_paths, labels)
-        self.cnn.create_model()
-        self.cnn.train()
-
-
-class Evaluate:
-    def __init__(self, model_info):
-        model_info["input_layer_batch_size"] = 1
-        self.cnn = CnnModel(model_info)
-
-    def evaluate_is_num(self, image_path):
-        self.cnn.create_batch([image_path], [0])
-        self.cnn.create_model()
-        output = self.cnn.sess.run(tf.arg_max(self.cnn.output, 1))
-        print(output)
-        return int(output)
-
-    def find_num_position(self, slice_image_path):
-        image = self.resize_image(slice_image_path)
-        slice_images = []
-        slice_image_paths = []
-        slice_image_position = []
-        for x in range(int(image.size[0] / 20)):
-            for y in range(int(image.size[1] / 20)):
-                slice_images.append(image.crop((x * 20, y * 20, (x + 1) * 20, (y + 1) * 20)))
-                slice_image_position.append([x * 20, y * 20])
-        for i in range(len(slice_images)):
-            slice_image_path = r'.\data\temp\\' + str(i) + ".jpg"
-            slice_image_paths.append(slice_image_path)
-            slice_images[i].save(slice_image_path)
-        self.cnn.model_info["input_layer_batch_size"] = len(slice_image_paths)
-        self.cnn.create_batch(slice_image_paths, [0] * len(slice_image_paths), False)
-        self.cnn.create_model()
-        output = list(self.cnn.sess.run(tf.arg_max(self.cnn.output, 1)))
-        show_square_image = array(image)
-        most_vote = [0] * int(image.size[1] / 20)
-        for y in range(int(image.size[1] / 20)):
-            for x in range(int(image.size[0] / 20)):
-                if output[x * int(image.size[1] / 20) + y] == 1:
-                    most_vote[y] += 1
-        most_vote_y = most_vote.index(max(most_vote))
-        print(most_vote)
-        print(most_vote.index(max(most_vote)))
-        for i in range(len(show_square_image[most_vote_y])):
-            show_square_image[most_vote_y * 20][i] = [255, 0, 0]
-            show_square_image[most_vote_y * 20 + 1][i] = [255, 0, 0]
-            show_square_image[most_vote_y * 20 + 60][i] = [255, 0, 0]
-            show_square_image[most_vote_y * 20 + 60 + 1][i] = [255, 0, 0]
-        # for i in range(len(output)):
-        #     if output[i] == 1:
-        #         for x in range(slice_image_position[i][0], slice_image_position[i][0] + 20):
-        #             for y in range(slice_image_position[i][1], slice_image_position[i][1] + 20):
-        #                 if x >= image.size[0] or y >= image.size[1]:
-        #                     continue
-        imshow(show_square_image)
-        show()
-        for i in slice_image_paths:
-            os.remove(i)
-        return list(output)
-
-    @staticmethod
-    def resize_image(image_path, mwidth=960, mheight=720):
-        image = Image.open(image_path)
-        w, h = image.size
-        if w <= mwidth and h <= mheight:
-            return image
-        if (1.0 * w / mwidth) > (1.0 * h / mheight):
-            scale = 1.0 * w / mwidth
-            new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-
-        else:
-            scale = 1.0 * h / mheight
-            new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-
-        return new_im
-
-
-if __name__ == "__main__":
-    cnn = Evaluate(MODEL_INFO)
-    cnn.find_num_position(r'.\data\test_images\IMG_7945.JPG')
-    # cnn.evaluate_is_num(r'.\data\temp\1027.jpg')
-    # trainer = Trainer(MODEL_INFO)
-    # trainer.start_train()
---
+++ evaluate.py
@@ -0,0 +1,70 @@
+import CnnModel
+import tensorflow as tf
+from PIL import Image
+from pylab import *
+import random
+import os
+
+
+class Evaluate:
+    def __init__(self, model_info):
+        self.batch_size = 1000
+        model_info['input_layer_batch_size'] = self.batch_size
+        self.cnn = CnnModel.CnnModel(model_info)
+        self.cnn.create_model()
+
+    def evaluate_is_num(self, image_path):
+        self.cnn.create_batch([image_path], [0])
+        self.cnn.create_model()
+        output = self.cnn.sess.run(tf.arg_max(self.cnn.output, 1))
+        print(output)
+        return int(output)
+
+    def find_num_position(self, slice_image_path):
+        input_image_shape = self.cnn.model_info['input_layer_image_shape']
+        image = self.resize_image(slice_image_path)
+        # image = Image.open(slice_image_path)
+        slice_images = []
+        slice_image_position = []
+        for i in range(self.batch_size):
+            x = random.randint(0, image.size[0] - input_image_shape[0])
+            y = random.randint(0, image.size[1] - input_image_shape[1])
+            slice_image = np.array(image.crop((x, y, x + input_image_shape[0], y + input_image_shape[1])).convert("L"))
+            slice_image = slice_image[:, :, np.newaxis]
+            slice_images.append(slice_image)
+            slice_image_position.append([x, y])
+        output = list(self.cnn.sess.run(tf.arg_max(self.cnn.output, 1), feed_dict={self.cnn.image_batch: slice_images}))
+
+        show_square_image = np.array(image)
+        for xy in range(len(slice_image_position)):
+            if output[xy] == 1:
+                for x in range(slice_image_position[xy][0], slice_image_position[xy][0] + input_image_shape[0]):
+                    for y in range(slice_image_position[xy][1], slice_image_position[xy][1] + input_image_shape[1]):
+                        show_square_image[y][x][0] += 1
+                        show_square_image[y][x][1] = 0
+                        show_square_image[y][x][2] = 0
+        imshow(show_square_image)
+        show()
+
+        return list(output)
+
+    @staticmethod
+    def resize_image(image_path, mwidth=960, mheight=720):
+        image = Image.open(image_path)
+        w, h = image.size
+        if w <= mwidth and h <= mheight:
+            return image
+        if (1.0 * w / mwidth) > (1.0 * h / mheight):
+            scale = 1.0 * w / mwidth
+            new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
+        else:
+            scale = 1.0 * h / mheight
+            new_im = image.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
+
+        return new_im
+
+
+if __name__ == "__main__":
+    cnn = Evaluate(CnnModel.MODEL_INFO)
+    for f in os.listdir(r'.\data\test_images'):
+        cnn.find_num_position(r'.\data\test_images\\' + f)
